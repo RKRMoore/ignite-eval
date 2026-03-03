cf58e011279a159a11be31fa7645ee27d3b73cdc
RKRMoore <moore@trfacilityservices.com>
2026-03-03 09:19:08 -0600

    Delete src directory

--- src/src/App.jsx
+++
@@ -1,4 +0,0 @@
-jsx<div style={S.logoWrap}>
-  <img src="/IPL_Logo__standard_.jpg" alt="Ignite" style={{ height:48, width:"auto", objectFit:"contain" }} />
-  <div style={S.logoSub}>Athlete Evaluation System</div>
-</div>
