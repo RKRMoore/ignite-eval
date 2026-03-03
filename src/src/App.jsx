567765cbf21b23b519cb4d1ffcafb686180b836e
RKRMoore <moore@trfacilityservices.com>
2026-03-03 08:19:49 -0600

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,4 @@
+jsx<div style={S.logoWrap}>
+  <img src="/IPL_Logo__standard_.jpg" alt="Ignite" style={{ height:48, width:"auto", objectFit:"contain" }} />
+  <div style={S.logoSub}>Athlete Evaluation System</div>
+</div>
